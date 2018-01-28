b4115a79f44beb2a964229f4ce19c9b3c8fc97d7
king <764666894@qq.com>
2018-01-28 14:52:50 +0800

    第三章第一题

--- my_opencv_demo/源.cpp
+++ my_opencv_demo/源.cpp
@@ -1,4 +1,7 @@
 #include "highgui.h"
+#include "cv.h"
+#include <iostream>
+using namespace std;
 void showpic(char * pic)
 {
 	IplImage * img = cvLoadImage(pic);
@@ -22,9 +25,26 @@ void aviplayer(char * video)
 	}
 		cvReleaseCapture(&capture);
 }
+void cal_example()//第三章练习题1
+{
+	double d = -2.501;
+	cout <<"value："<<d << " abs:" << CV_IABS(d) << "\tround:" << cvRound(d) << endl;//cvround四舍五入
+	CvRNG rng = cvRNG(cvGetTickCount());//时间戳作为seed
+	for (int i = 0; i < 10; i++)
+	{
+		cout << "random number["<<i<<"]:"<<cvRandInt(&rng) % 256;
+		cout << '\t' << cvRandReal(&rng) << endl;//随机数0-1
+	}
+	CvPoint2D32f point = cvPoint2D32f(1.22, 29.2);
+	CvPoint point1 = cvPointFrom32f(point);
+	CvPoint2D32f  point2 = cvPointTo32f(point1);
+	cout <<"Three point\t" <<'('<<point.x << ',' << point.y <<')'<< ' ' << '(' << point1.x << ',' << point1.y<< ')' <<
+		' ' << '(' << point2.x <<  ',' << point2.y<<')';
+}
 int main()
 {
 	//showpic("D:\\1.jpg");
-	aviplayer("D:\\1.avi");
+	//aviplayer("D:\\1.avi");
+	cal_example();
 	return 0;
 }
